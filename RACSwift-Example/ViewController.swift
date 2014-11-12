4aba0c6c3154e53fd76c048febe9971d41c85671
Azri Jamil <azri@nematix.com>
2014-11-12 10:46:52 +0800

    Initial Commit

---
+++ RACSwift-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RACSwift-Example
+//
+//  Created by Azri Jamil on 11/12/14.
+//  Copyright (c) 2014 Nematix System. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RACSwift-ExampleTests/RACSwift_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  RACSwift_ExampleTests.swift
+//  RACSwift-ExampleTests
+//
+//  Created by Azri Jamil on 11/12/14.
+//  Copyright (c) 2014 Nematix System. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RACSwift_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
